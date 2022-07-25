cdf56a0a4130ff1409f4816621e64d031713d382
Lee Lawton <lee@attaro.co.uk>
2022-07-25 09:17:23 +0100

    Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,6 @@ module.exports = {
         trackingIds: [
           "G-FWKZCFT0BK", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
